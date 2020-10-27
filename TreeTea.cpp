663d152075ee9e8b86a143d532ad7a52434adfcc
Ruinso <73478082+Ruinso@users.noreply.github.com>
2020-10-27 17:36:38 -0400

    TreeTeaProgram

--- TreeTea.cpp
+++ TreeTea.cpp
@@ -4,11 +4,11 @@
 using namespace std;
 
 class TreeNode{
-
+//Note that even though this is a basic program I fully understand
   public:
-
-    TreeNode(TreeNode* p, string k, string price)
-    :parent{p}, key{k}, value{price}
+	//create Tree structure
+    TreeNode(TreeNode* p, string k, string classification)
+    :parent{p}, key{k}, value{classification}
     {
       childL = nullptr;
       childR = nullptr;
@@ -20,33 +20,38 @@ class TreeNode{
     string value;
 };
 
-TreeNode* gpuTree;
+TreeNode* gpuTree;//create gputree
 
-void in_order_tree_walk(TreeNode* root){
-  if(root != nullptr){
-    in_order_tree_walk(root->childL);
-    cout << root->key << '\n';
-    in_order_tree_walk(root->childR);
+void in_order_tree_walk(TreeNode* father){
+  if(father != nullptr){//if the father node has a value other than null
+    in_order_tree_walk(father -> childL);//point to the left child value
+    cout << father -> key << '\n';//print out the key to the node
+    in_order_tree_walk(father -> childR);//point to the right child value
   }
 }
-
+//Main method
 int main(){
-  TreeNode* node = new TreeNode(nullptr, "gtx 1080ti", "high-end");
-  gpuTree = node;
-  TreeNode* temp = gpuTree;
-  node = new TreeNode(temp , "rtx 2080", "ultra high-end");
-  temp -> childL = node;
-  node = new TreeNode(temp, "gtx 1070", "old mid-range");
-  temp -> childR = node;
-  temp = node;
+  TreeNode* node = new TreeNode(nullptr, "gtx 1080ti", "high-end");//creates root value
+  gpuTree = node;//establishes this as the base node
+  TreeNode* temp = gpuTree;//creates an object that can be used to easily create and declare parents
+  node = new TreeNode(temp , "rtx 2080", "ultra high-end");//left child value is greater than parent
+  temp -> childL = node;//point to the left child node and assign the above information
+  node = new TreeNode(temp, "gtx 1070", "old mid-range");//right child value is less than parent
+  temp -> childR = node;//point the the right child node and assign the information
+  temp = node;//create new parent node using gtx 1070
   node = new TreeNode(temp, "gtx 1050ti", "low-range");
   temp -> childR = node;
   node = new TreeNode(temp, "gtx 1660", "mid-range");
   temp -> childL = node;
-  temp = node;
+  temp = node;//makes gtx 1660 parent
   node = new TreeNode(temp, "gtx 970", "old mid-range");
   temp -> childR = node;
   node = new TreeNode(temp, "gtx 780", "old high-end");
+  temp -> childL = node;
+  temp = node;//makes gtx 780 new parent
+  node = new TreeNode(temp, "gtx 750", "old low-end");
+  temp -> childR = node;
+  node = new TreeNode(temp, "gtx 770", "old mid-range");
   temp -> childL = node;
   in_order_tree_walk(gpuTree);
 }
